6899ee9a54e7df46426f7b6454517ff07ccd4405
Volodymyr <vladimir.selevertov@gmail.com>
2024-08-15 16:49:17 +0300

    added relationship for the Recipe model

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -4,8 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Recipe extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'title', 'description', 'instructions'];
+
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function ingredients() : BelongsToMany
+    {
+        return $this->belongsToMany(Ingredient::class)->withPivot('quantity');
+    }
+
+    public function categories() : BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function comments() : HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function favorites() : HasMany
+    {
+        return $this->hasMany(Favorite::class);
+    }
+
 }
