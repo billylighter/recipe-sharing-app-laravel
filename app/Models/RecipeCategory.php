d9fd9b46dd1ae27522325fa2fa73aad694b86731
Volodymyr <vladimir.selevertov@gmail.com>
2024-08-15 17:18:15 +0300

    added relationship for the RecipeCategory model

--- app/Models/RecipeCategory.php
+++ app/Models/RecipeCategory.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class RecipeCategory extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['recipe_id', 'category_id'];
+
+    /**
+     * Get the recipe associated with this relationship.
+     */
+    public function recipe(): BelongsTo
+    {
+        return $this->belongsTo(Recipe::class);
+    }
+
+    /**
+     * Get the category associated with this relationship.
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
