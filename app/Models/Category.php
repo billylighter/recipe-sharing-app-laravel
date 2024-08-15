c8f02f6fd2e1d8da6d3728840dbc944f6e7dea62
Volodymyr <vladimir.selevertov@gmail.com>
2024-08-15 17:10:30 +0300

    added relationship for the Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name'];
+
+    /**
+     * The recipes that belong to the category.
+     */
+    public function recipes() : BelongsToMany
+    {
+        return $this->belongsToMany(Recipe::class);
+    }
 }
