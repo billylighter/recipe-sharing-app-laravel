40242f08c25b9e2a2846ba7045a7dbaac94d65d1
Volodymyr <vladimir.selevertov@gmail.com>
2024-08-15 19:32:37 +0300

    added factory for Favorite model

--- database/factories/FavoriteFactory.php
+++ database/factories/FavoriteFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Recipe;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,8 @@ class FavoriteFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::all()->random()->id,
+            'recipe_id' => Recipe::factory()
         ];
     }
 }
