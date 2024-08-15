55e87a7330c5538ed8ba21a0804476436132b81e
Volodymyr <vladimir.selevertov@gmail.com>
2024-08-15 17:46:36 +0300

    added relationship for the RecipeIngredient model

--- app/Models/RecipeIngredient.php
+++ app/Models/RecipeIngredient.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class RecipeIngredient extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['recipe_id', 'ingredient_id', 'quantity'];
+
+    /**
+     * Get the recipe associated with this pivot entry.
+     */
+    public function recipe() : BelongsTo
+    {
+        return $this->belongsTo(Recipe::class);
+    }
+
+    /**
+     * Get the ingredient associated with this pivot entry.
+     */
+    public function ingredient() : BelongsTo
+    {
+        return $this->belongsTo(Ingredient::class);
+    }
 }
